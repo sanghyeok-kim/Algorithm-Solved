80befaa9eb1201b42becfca6b5e5dd26fbfe9ac4
sanghyeok-kim <slsnsi1212@gmail.com>
2022-01-16 23:24:20 +0900

    Solve 백준-2675-문자열반복

---
+++ 백준-2675-문자열반복/백준-2675-문자열반복/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  백준-2675-문자열반복
+//
+//  Created by 김상혁 on 2021/12/10.
+//
+
+import Foundation
+
+let inputCount = Int(readLine()!)!
+
+for i in 0..<inputCount {
+    let iterationCount = readLine()!.map{ Int(String($0)) }
+    let str = String(readLine()!)
+    
+    var temp: String
+    
+    for j in 0..<str.count {
+        temp += str
+    }
+}
