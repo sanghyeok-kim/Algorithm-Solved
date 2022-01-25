e935578f01ee9792b3f6a87246ae7955abab553d
sanghyeok-kim <slsnsi1212@gmail.com>
2022-01-25 23:58:38 +0900

    Solve 백준-8958-OX퀴즈

---
+++ 백준-8958-OX퀴즈/백준-8958-OX퀴즈/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  백준-8958-OX퀴즈
+//
+//  Created by 김상혁 on 2021/12/05.
+//
+
+import Foundation
+
+let row = Int(readLine()!)!
+var scores = [Int]()
+
+for i in 0..<row {
+    
+    let col = readLine()!.split(separator: "X").reduce(0) {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+print(col)
+
